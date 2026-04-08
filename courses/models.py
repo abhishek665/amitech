94105d253e4c071c5cc942c8cc290de6f4d9f13b
= <=>
2026-04-08 16:09:24 +0530

    course

--- courses/models.py
+++ courses/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class CourseCategory(models.Model):
     course_category = models.CharField(max_length=255, blank=True, null=True)
 
+    def __str__(self):
+        return self.course_category
+
 
 class Course(models.Model):
 
